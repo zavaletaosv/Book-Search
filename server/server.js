63e00f307cc42e64a7e07dff4f168feb20f6be34
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-22 19:01:06 -0500

    server/server.js updated: required ApolloServer, schemas, and authMiddleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const routes = require('./routes');
 
