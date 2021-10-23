d3b605d9d4f9bc4bf625d2e47a1e93d68b69941c
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-22 19:53:23 -0500

    server/schemas/typeDefs.js updated: auth type created

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql `
+const typeDefs = gql`
     type User {
         _id: ID
         username: String!
@@ -16,6 +16,11 @@ const typeDefs = gql `
         title: String!
         image: String!
         link: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
     }
 `;
 
