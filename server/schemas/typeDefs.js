5d03b4fa453af82bb5d0997c7ef3b7b7c779cb1e
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-22 19:56:59 -0500

    server/schemas/typeDefs.js updated: mutation type created

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,6 +26,13 @@ const typeDefs = gql`
     type Query {
         me: User
     }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: SavedBookInput): User
+        removeBook(bookId: String!): User
+    }
 `;
 
 module.exports = typeDefs;
