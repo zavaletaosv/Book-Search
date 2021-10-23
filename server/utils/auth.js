b51905a9e9cb7e614b12b7473d7aa8eb82db52d7
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-22 19:19:47 -0500

    server/utils/auth.js updated: auth middleware function updated to work with graphql

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,9 +12,10 @@ module.exports = {
   },
 
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function (...props) {
+    const { req, res, next } = props[0]
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -22,7 +23,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
