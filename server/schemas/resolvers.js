a378aff50c1f7261504d8ee74cb877efbd636ad6
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-22 19:34:57 -0500

    server/schemas/resolvers.js updated: me query created

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,4 +2,16 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-errors');
 
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select("-__v -password");
+                return userData;
+            }
+            throw new AuthenticationError("Cannot find this user!");
+        },
+    },
+};
+
 module.exports = resolvers;
