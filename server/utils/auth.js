d511f0a16008870a8d8d10be2e11dc064ba1ef98
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-22 19:15:08 -0500

    server/utils/auth.js updated: signToken moved

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,12 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -25,15 +31,8 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return req;
   },
 };
